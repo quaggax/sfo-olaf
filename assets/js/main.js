4625c007a793e943c7a0094875f9fe671315311e
quaggax <122413643+quaggax@users.noreply.github.com>
2023-03-19 23:01:24 +0100

    fixed logo stretch on ipad 12.9 in portrait mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,9 @@ function ExternalLinks() {
     for (var i = 0; i < anchorElements.length; i++) {
         var anchorElement = anchorElements[i];
         anchorElement.getAttribute("href") && anchorElement.hostname !== location.hostname && (anchorElement.target = "_blank");
+        /* the && (logical AND) returns the value of the first 'false' expression (eg. empty string); if all expressions are true it returns the value of the last one. if there is a 'false' expression the rest of the statement isn't even called.
+        read more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND#short-circuit_evaluation */
+        /* in this case the statements checks if the href attribute even has a value. if yes, the code comapres the anchors hostname to the current pages hostname. if they are different, the statement between the parenthesis is returned. this sets the target attribute to "_blank". */
     }
 }
 ExternalLinks();
