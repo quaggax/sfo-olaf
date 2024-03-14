9f91055dc057008f1924618ce421eaba2eb08c89
quaggax <122413643+quaggax@users.noreply.github.com>
2024-03-14 23:20:39 +0100

    adjustments for MODX implementation
    * improved category/datetime/location bar layout on pages and cards
    * replaced all icons to use provided classes instead of HTML codes
        - removed ._bootstrap-icons class
    * removed .announcement-alert in favor of universal .olaf-alert use

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.setServerOptions({
+        port: 55660,
+        showAllHosts: true, // Set this to true to access the server on your local network
+    });
+
     eleventyConfig.addPassthroughCopy("./assets/.");
     eleventyConfig.addPassthroughCopy({ "./node_modules/bootstrap-icons/font/fonts/.": "assets/fonts/bootstrap-icons" })
 
